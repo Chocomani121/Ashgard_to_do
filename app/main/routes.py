c5f5e600303014f9ffe3dc9ae5b1e8251ad73e74
pgenon53@gmail.com <pgenon53@gmail.com>
2026-01-07 16:25:59 +0800

    This forces the browser to check the server every time the back button is cliked

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,33 +1,49 @@
 from flask import render_template, Blueprint
+from flask_login import login_required 
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
-@main.route("/projects") 
-def projects():
-    return render_template('index.html')
+@login_required
+def login():
+    return render_template('login.html')
 
-@main.route("/tasks") 
+@main.route("/tasks")
+@login_required 
 def tasks():
     return render_template('tasks.html', title="Tasks Info")
 
-@main.route("/departments") 
+@main.route("/departments")
+@login_required 
 def departments():
     return render_template('all_departments.html')
 
 @main.route("/members")
+@login_required
 def members():
     return render_template('members.html', title="Members")
 
 @main.route("/project_details")
+@login_required
 def project_details():
     return render_template('project_details.html')
 
 @main.route("/profile")
+@login_required
 def profile():
     return render_template('profile.html')
 
 @main.route("/all_departments")
+@login_required
 def all_departments():
     return render_template('all_division.html')
 
+@main.route("/projects")
+@login_required
+def projects():  # Make sure this name only appears ONCE in this file
+    return render_template('layout.html')
+
+@main.route("/dashboard")
+@login_required
+def dashboard():  # This function must have a different name
+    return render_template('layout.html')
