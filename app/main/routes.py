2d50e3705ae97972c7a14fc8aa331e6e71e72306
Anen0 <Lum3nGee@gmail.com>
2026-01-08 16:50:18 +0800

    ---local--pull

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,70 +1,36 @@
 from flask import render_template, Blueprint
+from flask_login import login_required
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/projects") 
+@login_required
 def projects():
     return render_template('index.html')
 
 @main.route("/tasks") 
+@login_required
 def tasks():
     return render_template('tasks.html', title="Tasks Info")
 
 @main.route("/departments") 
-def all_departments():
-    return render_template('all_departments.html')
-
-@main.route("/members")
-def members():
-    return render_template('members.html', title="Members")
-
-@main.route("/project_details")
-def project_details():
-    return render_template('project_details.html')
-
-@main.route("/profile")
-def profile():
-    return render_template('profile.html')
-
-@main.route("/all_departments")
-def all_departments():
-    return render_template('all_departments.html')
-
-@main.route("/task details")
-def task_details():
-    return render_template('task_details.html')
-
-@main.route("/department")
+@login_required
 def departments():
-    return render_template('departments.html')
-
-from flask import render_template, Blueprint
-
-main = Blueprint('main', __name__)
-
-@main.route("/")
-@main.route("/projects") 
-def projects():
-    return render_template('index.html')
-
-@main.route("/tasks") 
-def tasks():
-    return render_template('tasks.html', title="Tasks Info")
-
-@main.route("/departments") 
-def all_departments():
     return render_template('all_departments.html')
 
 @main.route("/members")
+@login_required
 def members():
     return render_template('members.html', title="Members")
 
 @main.route("/project_details")
+@login_required
 def project_details():
     return render_template('project_details.html')
 
 @main.route("/profile")
+@login_required
 def profile():
     return render_template('profile.html')
 
