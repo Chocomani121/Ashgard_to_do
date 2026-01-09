67bb7985c0d4a00faa31a803d0ec7812e7e3736f
bards <barsdsjaverle2@gmail.com>
2026-01-09 10:36:29 +0800

    updated branch

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,26 +3,6 @@ from flask_login import login_required
 
 main = Blueprint('main', __name__)
 
-
-@main.route("/projects") 
-@login_required
-def projects():
-    return render_template('index.html')
-
-@main.route("/tasks") 
-@login_required
-def tasks():
-    return render_template('tasks.html', title="Tasks Info")
-
-@main.route("/departments") 
-@login_required
-def departments():
-    return render_template('departments.html')
-
-from flask import render_template, Blueprint
-
-main = Blueprint('main', __name__)
-
 @main.route("/")
 @main.route("/projects") 
 @login_required
@@ -60,4 +40,3 @@ def profile():
 @login_required
 def task_details():
     return render_template('task_details.html')
-
