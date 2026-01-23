37f3f075e7972cabb3dd2baa9931896ee042fc97
pgenon53@gmail.com <pgenon53@gmail.com>
2026-01-23 15:54:34 +0800

    load_dotenv()

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
-import urllib.parse 
+import os
+import urllib.parse
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
 
+load_dotenv()
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -14,17 +17,35 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 login_manager.login_message = None
 mail = Mail()
-migrate = Migrate() 
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    
-    
+
+    user = os.getenv("DB_USER")
+    raw_password = os.getenv("DB_PASSWORD")
+    password = urllib.parse.quote_plus(raw_password)
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+    app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
+    app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS") == "True"
+    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+    app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    migrate.init_app(app, db) 
+    migrate.init_app(app, db)
 
     @app.after_request
     def add_header(response):
@@ -33,10 +54,9 @@ def create_app():
         response.headers["Expires"] = "0"
         return response
 
-    
     from .users.routes import users as users_blueprint
     from .main.routes import main as main_blueprint
-    
+
     app.register_blueprint(users_blueprint)
     app.register_blueprint(main_blueprint)
 
