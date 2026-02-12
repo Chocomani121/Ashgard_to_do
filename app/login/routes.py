e2974cc8ebcc50149af7a17a2d78e52d66494b45
Katsumi <kathrinalibres122002@gmail.com>
2026-02-12 10:28:19 +0800

    debugged routes

--- app/login/routes.py
+++ app/login/routes.py
@@ -30,7 +30,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('auth.projects'))
+            return redirect(next_page) if next_page else redirect(url_for('project.projects'))
         form.email.errors.append('Invalid email or password.')
     return render_template('auth/auth-login.html', title='Login', form=form)
 
