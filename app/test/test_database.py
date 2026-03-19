97571dc1878cfe456fe3f76ea47fadea25fe677d
Anen0 <Lum3nGee@gmail.com>
2026-03-19 16:49:43 +0800

    simple bg testing done

--- app/test/test_database.py
+++ app/test/test_database.py
@@ -5,10 +5,10 @@ from celery.result import AsyncResult
 
 @test_bp.route('/testtask', methods=['GET', 'POST'])
 def test_task():
-    if request.method == 'POST':
-        testtag = request.form['test_db_connection']
-        print(f"\n\n\nTest Tag: {testtag}\n\n\n")
-        flash(f"Test Tag: {testtag}" , "info")
+    # if request.method == 'POST':
+    #     testtag = request.form['test_db_connection']
+    #     print(f"\n\n\nTest Tag: {testtag}\n\n\n")
+    #     flash(f"Test Tag: {testtag}" , "info")
 
     return render_template('test_page.html')
 
@@ -43,24 +43,26 @@ def connect_remote_db():
         progbar = request.form.get('progbar')
 
         
-        # if progbar:
-        # ----POLLING/Non-blocking
-        print(f"\n\n :D \n\n")
-        if "application/json" in (request.headers.get("Accept") or ""):
-            return jsonify(task_id=task.id)
-        flash(f"Task queued. Task ID: {task.id}", "info")
-        # else:
-        #     # ----Blocking
-        #     result  = task.get(timeout=10)
+        if progbar:
+            # ----POLLING/Non-blocking
+            print(f"\n\n :D \n\n")
+            if "application/json" in (request.headers.get("Accept") or ""):
+                return jsonify(task_id=task.id)
+            flash(f"Task queued. Task ID: {task.id}", "info")
+            
+        else:
+            # ----Blocking
+            result  = task.get(timeout=10)
 
-        #     if result['success']:
-        #         print(f"\n\n {result} \n\n")
-        #         flash(f"WAITING: {result['message']}", "success")
-        #     else:
-        #         flash(f"Remote DB connection test stopped. Task ID: {task.id}", "danger")
+            if result['success']:
+                print(f"\n\n {result} \n\n")
+                flash(f"WAITING: {result['message']}", "success")
+            else:
+                flash(f"Remote DB connection test stopped. Task ID: {task.id}", "danger")
+        
+        return redirect(url_for("test_bp.connect_remote_db"))
         
         
-        return redirect(url_for("test_bp.test_task"))
 
     return render_template('test_page.html')
 
